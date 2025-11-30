3b64ad0917a79f4cbb93957537c3382ec1c33bd4
Pau <paulina.floress@iteso.mx>
2025-11-29 18:24:31 -0600

    Implementacion del automata de pila

--- parser.py
+++ parser.py
@@ -2,10 +2,50 @@
 import ply.yacc as yacc
 from lexer import tokens # importar los tokens definidos en lexer.py
 
+# 1.5 Implementación del automáta de pila
+# Se crea la pila que va a almacenar los valores del AP
+AP = []
+
+# Función para validar si el símbolo de cierre es el correcto (dependiendo del de inicio)
+def validar_simbs(sim_cierre):
+    if sim_cierre == '}':
+        sim_inicio = '{'
+    elif sim_cierre == ']':
+        sim_inicio = '['
+    else:
+        print("\n❗Símbolo no válido")
+
+    # Si hay un símbolo de cierre sin que haya uno inicial
+    if not AP:
+        print("\n❌ Error en el AP: No puede haber un símbolo de cierre sin uno de apertura")
+
+    # En base al último símbolo guardado en la pila... 
+    tope_AP = AP[-1]
+
+    # Se desapila el símbolo final si coincide (es el par) del inicial
+    if tope_AP == sim_inicio:
+        AP.pop()
+        print(f"↓ Despilando símbolo '{tope_AP}'")
+
+    else:
+        print(f"\n❌ Error en el AP: El símbolo de cierre no concuerda con el último cierre almacenado")
+
 #2. Definición de las funciones asociadas con las reglas de producción
 def p_S(p):
     'S : LKEY A RKEY'
+
+    AP.append('{') # se agrega { al AP
+    print("↑ Apilando símbolo '{'")
+
     p[0] = (p[1], p[3])
+
+    validar_simbs('}') # se desapila el símbolo }
+
+    # Revisa si quedan símbolos en el AP una vez terminada la ejecución
+    if len(AP) == 0:
+        print("\n✅ Automáta de Pila finalizado correctamente! La pila está vacía")
+    else:
+        print("\n❌ Error en el AP: Quedan símbolos en el AP, por lo que el automáta no es válido")
 
 def p_folio(p):
     'A : FOLIO COLON NUM_FOLIO COMMA B'
@@ -21,7 +61,13 @@ def p_fecha_validacion(p):
 
 def p_paciente(p):
     'D : PACIENTE COLON LKEY E RKEY COMMA I'
+
+    AP.append('{') # se agrega { al AP
+    print("↑ Apilando símbolo '{'")
+
     p[0] = (p[1], p[2], p[3], p[5], p[6])
+
+    validar_simbs('}') # se desapila el símbolo }
 
 def p_nombre(p):
     'E : NOMBRE COLON CADENA COMMA F'
@@ -49,11 +95,23 @@ def p_seccion(p):
 
 def p_parametros(p):
     'K : PARAMETROS COLON LTKEY O RTKEY COMMA L'
+
+    AP.append('[') # se agrega [ al AP
+    print("↑ Apilando símbolo '['")
+
     p[0] = (p[1], p[2], p[3], p[4])
+
+    validar_simbs(']') # se desapila el símbolo ]
 
 def p_firma(p):
     'L : FIRMA COLON LKEY M RKEY'
+
+    AP.append('{') # se agrega { al AP
+    print("↑ Apilando símbolo '{'")
+
     p[0] = (p[1], p[2], p[3], p[5])
+
+    validar_simbs('}') # se desapila el símbolo }
 
 def p_responsable(p):
     'M : RESPONSABLE COLON CADENA COMMA N'
@@ -73,35 +131,83 @@ def p_lista_parametros_simple(p):
 
 def p_parametro_leucocitos(p):
     'PARAMETRO : LKEY NOMBRE COLON LEUCOCITOS COMMA RESULTADO COLON NUM_RESULTADO COMMA UNIDAD COLON SIMBOLO_UNIDAD_MCPL COMMA LIMITE COLON LIMITE_VALUES P RKEY'
+    
+    AP.append('{') # se agrega { al AP
+    print("↑ Apilando símbolo '{'")
+
     p[0] = ('leucocitos', p[4], p[8], p[12], p[16], p[17])
+
+    validar_simbs('}') # se desapila el símbolo }
 
 def p_parametro_eritrocitos(p):
     'PARAMETRO : LKEY NOMBRE COLON ERITROCITOS COMMA RESULTADO COLON NUM_RESULTADO COMMA UNIDAD COLON SIMBOLO_UNIDAD_MCPL COMMA LIMITE COLON LIMITE_VALUES P RKEY'
+    
+    AP.append('{') # se agrega { al AP
+    print("↑ Apilando símbolo '{'")
+
     p[0] = ('eritrocitos', p[4], p[8], p[12], p[16], p[17])
+
+    validar_simbs('}') # se desapila el símbolo }
 
 def p_parametro_hemoglobina(p):
     'PARAMETRO : LKEY NOMBRE COLON HEMOGLOBINA COMMA RESULTADO COLON NUM_RESULTADO COMMA UNIDAD COLON SIMBOLO_UNIDAD_GPD COMMA LIMITE COLON LIMITE_VALUES P RKEY'
+    
+    AP.append('{') # se agrega { al AP
+    print("↑ Apilando símbolo '{'")
+
     p[0] = ('hemoglobina', p[4], p[8], p[12], p[16], p[17])
+
+    validar_simbs('}') # se desapila el símbolo }
 
 def p_parametro_hematocrito(p):
     'PARAMETRO : LKEY NOMBRE COLON HEMATOCRITO COMMA RESULTADO COLON NUM_RESULTADO COMMA UNIDAD COLON SIMBOLO_UNIDAD_PORCENTAJE COMMA LIMITE COLON LIMITE_VALUES P RKEY'
+    
+    AP.append('{') # se agrega { al AP
+    print("↑ Apilando símbolo '{'")
+
     p[0] = ('hematocrito', p[4], p[8], p[12], p[16], p[17])
+
+    validar_simbs('}') # se desapila el símbolo }
 
 def p_parametro_plaquetas(p):
     'PARAMETRO : LKEY NOMBRE COLON PLAQUETAS COMMA RESULTADO COLON NUM_PLAQUETAS COMMA UNIDAD COLON SIMBOLO_UNIDAD_MCPL COMMA LIMITE COLON LIMITE_VALUES P RKEY'
+    
+    AP.append('{') # se agrega { al AP
+    print("↑ Apilando símbolo '{'")
+
     p[0] = ('plaquetas', p[4], p[8], p[12], p[16], p[17])
+
+    validar_simbs('}') # se desapila el símbolo }
 
 def p_parametro_neutrofilos(p):
     'PARAMETRO : LKEY NOMBRE COLON NEUTROFILOS COMMA RESULTADO COLON NUM_RESULTADO COMMA UNIDAD COLON SIMBOLO_UNIDAD_MCPL COMMA LIMITE COLON LIMITE_VALUES P RKEY'
+    
+    AP.append('{') # se agrega { al AP
+    print("↑ Apilando símbolo '{'")
+
     p[0] = ('neutrofilos', p[4], p[8], p[12], p[16], p[17])
+
+    validar_simbs('}') # se desapila el símbolo }
 
 def p_parametro_linfocitos(p):
     'PARAMETRO : LKEY NOMBRE COLON LINFOCITOS COMMA RESULTADO COLON NUM_RESULTADO COMMA UNIDAD COLON SIMBOLO_UNIDAD_MCPL COMMA LIMITE COLON LIMITE_VALUES P RKEY'
+    
+    AP.append('{') # se agrega { al AP
+    print("↑ Apilando símbolo '{'")
+
     p[0] = ('linfocitos', p[4], p[8], p[12], p[16], p[17])
+
+    validar_simbs('}') # se desapila el símbolo }
 
 def p_parametro_monocitos(p):
     'PARAMETRO : LKEY NOMBRE COLON MONOCITOS COMMA RESULTADO COLON NUM_RESULTADO COMMA UNIDAD COLON SIMBOLO_UNIDAD_MCPL COMMA LIMITE COLON LIMITE_VALUES P RKEY'
+    
+    AP.append('{') # se agrega { al AP
+    print("↑ Apilando símbolo '{'")
+
     p[0] = ('monocitos', p[4], p[8], p[12], p[16], p[17])
+
+    validar_simbs('}') # se desapila el símbolo }
 
 def p_nota_con_coma(p):
     'P : COMMA NOTA COLON SIMBOLOS_NOTA'
